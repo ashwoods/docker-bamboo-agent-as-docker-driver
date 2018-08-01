8d102abc405c9d155447d35141f2eb2c8501c400
Marcin Walerianczyk <mwalerianczyk@atlassian.com>
2018-08-01 10:01:58 +0200

    Copy bamboo-update-capability.sh script

---
+++ bamboo-update-capability.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# COPY FROM https://bitbucket.org/atlassian/bamboo-update-capability/raw/master/bamboo-update-capability.sh
+displayUsage() { 
+	echo "\nScript to upgrade bamboo agent capabilities directly in properties file"
+	echo "Will remove from file old property with given name and write a new one"
+	echo "\nUsage: $(basename "$0") capability value [file.properties]"
+	echo "\n  capability - capabaliyty name" 
+	echo "  value - value of capability" 
+	echo "  file.properties - optional path to properties file (default: ~/bamboo-agent-home/bamboo-capabilities.properties)\n" 
+} 
+
+if [  $# -le 1 ]; then 
+	displayUsage
+	exit 1
+fi 
+
+if [ -z "$3" ]; then
+	propertiesFilePath=~/bamboo-agent-home/bin/bamboo-capabilities.properties
+else
+	propertiesFilePath=$3
+fi
+
+#create file if missing
+if [ ! -f $propertiesFilePath ]; then
+  dir=$(dirname ${propertiesFilePath})
+  mkdir -p ${dir}
+  touch $propertiesFilePath
+fi
+
+#escaping ' ', '=' and '\'
+name=$(echo $1 | sed -e 's/\\/\\\\\\\\/g' -e 's/ /\\ /g' -e 's/=/\\=/g' )
+value=$(echo $2 | sed -e 's/\\/\\\\\\\\/g' -e 's/ /\\ /g' -e 's/=/\\=/g')
+#remove previous values of this key
+nameToRemove=$(echo $name | sed -e 's/\\/\\\\/g')
+sed -e "/^$nameToRemove=/d" -i $propertiesFilePath
+#add new value of this key
+echo $name=$value >> $propertiesFilePath
